d52de46f23fbb1eb32ee396c7d27bc59bc6ccb53
AlexReva27 <52403650+AlexReva27@users.noreply.github.com>
2019-07-05 11:24:30 +0700

    Update car_numbers.py

--- car_numbers.py
+++ car_numbers.py
@@ -1,27 +1,30 @@
-import os, keyboard
- 
+import os, keyboard, unittest
+
 file = open("base.txt").read()
- 
-def funс():
+
+
+def func():
     guess = input("Введите номер автомобиля или часть номера: ").strip()
     if len(guess) != 0:
         numbers = [word for word in file.split() if guess in word]
         if len(numbers) != 0:
             for x in numbers:
                 print(x)
-            print("Колличество найденых номеров: " + str(len(numbers)))            
+            print("Колличество найденых номеров: " + str(len(numbers)))
         else:
             print("Таких номеров нет в базе")
     else:
-        print("Вы ничего не ввели")   
-    print("\nДля нового поиска - нажмите Space.\nДля выхода из программы - нажмите Esc.")              
-    exit() 
- 
-def exit():
-    while True:   
+        print("Вы ничего не ввели")
+    print("\nДля нового поиска - нажмите Space.\nДля выхода из программы - нажмите Esc.")
+    ex()
+
+
+def ex():
+    while True:
         if keyboard.is_pressed('Esc'):
             break
-        if keyboard.is_pressed('space'):        
+        if keyboard.is_pressed('space'):
             funс()
 
-funс()
+
+func()
